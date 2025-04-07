9411c30bece18062d90970d110a304823a85ab6e
Arpan Kundu <134829639+arpankundu05@users.noreply.github.com>
2025-04-07 23:46:45 +0530

    Add files via upload

---
+++ DATA STRUCTURES ( BASICS )/14. QUEUE/Article_Video/1.Introduction.py
@@ -0,0 +1,35 @@
+# Queue data structure
+
+# What is Queue?
+"""
+A queue is a linear data structure that is open at both ends and the operations are performed in First In First Out (FIFO) order.
+
+We define a queue to be a list in which all additions to the list are made at one end, and all deletions from the list are made at the other end.  The element which is first pushed into the order, the operation is first performed on that.
+"""
+
+# Characteristics of Queue:
+"""
+→ Queue can handle multiple data.
+→ We can access both ends.
+→ They are fast and flexible. 
+"""
+
+# Types of Queue:
+"""
+There are different types of queue:
+
+● Input Restricted Queue
+This is a simple queue. In this type of queue, the input can be taken from only one end but deletion can be done from any of the ends.
+
+● Output Restricted Queue
+This is also a simple queue. In this type of queue, the input can be taken from both ends but deletion can be done from only one end.
+
+● Circular Queue
+This is a special type of queue where the last position is connected back to the first position. Here also the operations are performed in FIFO order.
+
+● Double-Ended Queue (Dequeue)
+In a double-ended queue the insertion and deletion operations, both can be performed from both ends.
+
+● Priority Queue
+A priority queue is a special queue where the elements are accessed based on the priority assigned to them.
+"""
